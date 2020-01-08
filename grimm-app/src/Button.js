3456414223a8f25ee22b8345d61c5bcb8741a56f
Adrian Grimm <usmcamgrimm@gmail.com>
2020-01-07 20:27:58 -0500

    React app, Day 3
    useEffects

--- grimm-app/src/Button.js
+++ grimm-app/src/Button.js
@@ -1,14 +1,33 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
+import "./App.js";
 
-function Button() {
+const useFetch = (url) => {
+    const [data, setData] = useState(null);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        async function fetchData() {
+            const response = await fetch (url);
+            const data = await response.json();
+            const [item] = data.results;
+            setData(item);
+            setLoading(false);
+        }
+        fetchData();
+    }, []);
+
+    return {data, loading};
+}
+
+export default () => {
     const [count, setCount] = useState(0);
+    const {data, loading} = useFetch("https://api.randomuser.me/");
 
     return (
         <div>
             <p>You clicked {count} times</p>
-            <button onClick={() => setCount(count + 1)}>Click</button>
+            <button onClick={() => setCount(count + 1)}>Click me</button>
+            {loading ? <div>...loading</div> : <div>{data.name.first}</div>}
         </div>
-    )
-}
-
-export default Button;
+    );
+};
--- grimm-app/src/NavBar.js
+++ grimm-app/src/NavBar.js
@@ -3,7 +3,7 @@ import React from 'react';
 function NavBar(props) {
     const navTabs = props.navTabs;
     const navItems = navTabs.map((navItem) => {
-        return <li className="navlist">{navItem}</li>
+        return <li className="navlist" key ={navItem}>{navItem}</li>
     });
     return <ul className="nav">{navItems}</ul>
 };
