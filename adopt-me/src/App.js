15aa4adc90c65f23e883b77f117d4f36dea1de0e
Adrian Grimm <usmcamgrimm@gmail.com>
2020-01-10 05:40:42 -0500

    Add Pet component to a new file

--- adopt-me/src/App.js
+++ adopt-me/src/App.js
@@ -1,18 +1,12 @@
 import React from "react";
-import { render } from "react-dom";
-
-const Pet = ({ name, animal, breed }) => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, name),
-    React.createElement("h2", {}, animal),
-    React.createElement("h2", {}, breed)
-  ]);
-};
+import {
+  render
+} from "react-dom";
+import Pet from "./Pet";
 
 const App = () => {
   return React.createElement(
-    "div",
-    {
+    "div", {
       id: "something-important"
     },
     [
