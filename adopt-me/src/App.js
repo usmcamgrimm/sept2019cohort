987134fbdce7feec47a62c6ca151fc01203a46ee
Adrian Grimm <usmcamgrimm@gmail.com>
2020-01-11 11:44:47 -0500

    Add hooks

--- adopt-me/src/App.js
+++ adopt-me/src/App.js
@@ -1,10 +1,10 @@
 import React from "react";
-import {
-  render
-} from "react-dom";
-import Pet from "./Pet";
+import { render } from "react-dom";
+// import Pet from "./Pet";
+import SearchParams from "./SearchParams";
 
 const App = () => {
+  /*
   return React.createElement(
     "div", {
       id: "something-important"
@@ -28,6 +28,18 @@ const App = () => {
       })
     ]
   );
+  */
+  return (
+    <div>
+      <h1 id="something-important">Adopt Me</h1>
+      {/*}
+      <Pet name="Jezebel" animal="Cat" breed="Persian" />
+      <Pet name="Jins" animal="Cat" breed="Himalayan" />
+      <Pet name="Brutus" animal="Dog" breed="Mastiff" />
+      */}
+      <SearchParams />
+    </div>
+  );
 };
 
-render(React.createElement(App), document.getElementById("root"));
+render(<App />, document.getElementById("root"));
--- adopt-me/src/Pet.js
+++ adopt-me/src/Pet.js
@@ -1,5 +1,5 @@
-import React from 'react'
-export default function Pet ({ name, animal, breed }) {
+import React from "react";
+export default function Pet({ name, animal, breed }) {
   // return React.createElement("div", {}, [
   //     React.createElement("h1", {}, name),
   //     React.createElement("h2", {}, animal),
@@ -12,5 +12,5 @@ export default function Pet ({ name, animal, breed }) {
       <h2> {animal} </h2>
       <h2> {breed} </h2>
     </div>
-  )
+  );
 }
