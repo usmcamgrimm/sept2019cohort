65d7d9fd3852814e90e057218ff6a217e7973f77
Adrian Grimm <usmcamgrimm@gmail.com>
2020-01-11 13:05:48 -0500

    Enabling strict mode
    Also delete these leftovers from an earlier portion of the lesson

--- adopt-me/src/App.js
+++ adopt-me/src/App.js
@@ -4,41 +4,18 @@ import { render } from "react-dom";
 import SearchParams from "./SearchParams";
 
 const App = () => {
-  /*
-  return React.createElement(
-    "div", {
-      id: "something-important"
-    },
-    [
-      React.createElement("h1", {}, "Adopt Me!"),
-      React.createElement(Pet, {
-        name: "Jinx",
-        animal: "Cat",
-        breed: "Himalayan"
-      }),
-      React.createElement(Pet, {
-        name: "Jezebel",
-        animal: "Cat",
-        breed: "Persian"
-      }),
-      React.createElement(Pet, {
-        name: "Brutus",
-        animal: "Dog",
-        breed: "Mastiff"
-      })
-    ]
-  );
-  */
   return (
-    <div>
-      <h1 id="something-important">Adopt Me</h1>
-      {/*}
+    <React.StrictMode>
+      <div>
+        <h1 id="something-important">Adopt Me</h1>
+        {/*
       <Pet name="Jezebel" animal="Cat" breed="Persian" />
       <Pet name="Jins" animal="Cat" breed="Himalayan" />
       <Pet name="Brutus" animal="Dog" breed="Mastiff" />
       */}
-      <SearchParams />
-    </div>
+        <SearchParams />
+      </div>
+    </React.StrictMode>
   );
 };
 
