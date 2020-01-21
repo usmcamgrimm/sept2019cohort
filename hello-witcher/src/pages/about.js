4db1e4bc0b519e8a617dbecc9d58c7b5a5bd1fe0
Adrian Grimm <usmcamgrimm@gmail.com>
2020-01-20 21:34:01 -0500

    <Link />

--- hello-witcher/src/pages/about.js
+++ hello-witcher/src/pages/about.js
@@ -1,8 +1,10 @@
 import React from "react"
+import { Link } from "gatsby"
 import Header from "../components/header"
 
 export default () => (
-  <div style={{ color: `green`, fontSize: `48px` }}>
+  <div style={{ color: `green` }}>
+    <Link to="//">Home</Link>
     <Header headerText="About the Gatsby-School Witchers" />
     <p>Such wow. React and witchers</p>
   </div>
--- hello-witcher/src/pages/index.js
+++ hello-witcher/src/pages/index.js
@@ -1,8 +1,10 @@
 import React from "react"
+import { Link } from "gatsby"
 import Header from "../components/header"
 
 export default () => (
   <div style={{ color: `#d50000` }}>
+    <Link to="/about/">About</Link>
     <Header headerText="The Witcher" />
     <p>What a world.</p>
   </div>
