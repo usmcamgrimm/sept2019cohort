d209213ae0f8f4fa5332cbe3cb1dbfe09d04f9f7
Adrian Grimm <usmcamgrimm@gmail.com>
2020-01-20 20:23:08 -0500

    gatsby start

---
+++ hello-witcher/src/pages/about.js
@@ -0,0 +1,8 @@
+import React from "react"
+
+export default () => (
+  <div style={{ color: `#d50000` }}>
+    <h1>About Gatsby-School Witchers</h1>
+    <p>Such wow. React and witchers</p>
+  </div>
+)
---
+++ hello-witcher/src/pages/index.js
@@ -0,0 +1,8 @@
+import React from "react"
+
+export default () => (
+  <div style={{ color: `#d50000` }}>
+    <h1>Hello Witcher!</h1>
+    <p>What a world.</p>
+  </div>
+)
