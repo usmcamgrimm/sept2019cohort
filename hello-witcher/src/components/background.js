bc1f0731f014ae2ac7a2832ea57dece9d5cbe4fd
Adrian Grimm <usmcamgrimm@gmail.com>
2020-01-22 06:29:45 -0500

    Remove background, work on landing page

--- hello-witcher/src/components/background.js
+++
@@ -1,6 +0,0 @@
-import React from "react"
-import backgroundStyles from "./background.module.css"
-
-export default ({ children }) => (
-  <div className={backgroundStyles.background}>{children}</div>
-)
--- hello-witcher/src/pages/index.js
+++ hello-witcher/src/pages/index.js
@@ -1,14 +1,22 @@
 import React from "react"
 import { Link } from "gatsby"
 import Header from "../components/header"
-import Background from "../components/background"
+import Container from "../components/container"
 
+{
+  /*
 export default () => (
-  <Background>
-    <div style={{ color: `#d50000` }}>
-      <Link to="/about/">About</Link>
-      <Header headerText="The Witcher" />
-      <p>The world of the Witchers, coming soon</p>
-    </div>
-  </Background>
+  <div style={{ color: `#d50000` }}>
+    <Link to="/about/">About</Link>
+    <Header headerText="The Witcher" />
+    <p>The world of the Witchers, coming soon</p>
+  </div>
+)
+*/
+}
+export default () => (
+  <Container>
+    <h1>The Witcher</h1>
+    <h2>Enter</h2>
+  </Container>
 )
